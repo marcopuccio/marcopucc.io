71c7ed90ca827f381015b34893fe67c3e95c43db
Marco Puccio <mpuccio90@gmail.com>
2021-05-02 15:34:45 -0300

    header component

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+import { Header } from "../components/Header";
+
+export const App: React.FC = () => {
+  return (
+    <div className="App-container">
+      <Header />
+    </div>
+  );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <div>avatar</div>
+      <div>name</div>
+    </header>
+  );
+};
