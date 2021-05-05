40ac621f33b6baae52964ab1078cd337c28e186c
Marco Puccio <mpuccio90@gmail.com>
2021-05-05 20:50:18 -0300

    improve page styles

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -8,12 +8,16 @@ import { Menu } from "../components/Menu";
 export const App: React.FC = () => {
   return (
     <div className="App-container">
-      <section className="mt-5">
-        <Header />
-      </section>
-      <section className="my-2">
-        <Menu />
-      </section>
+      <div className="card">
+        <div className="card-content">
+          <div className="media">
+            <Header />
+          </div>
+          <div className="content">
+            <Menu />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
