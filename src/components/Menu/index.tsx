024dd6fb8745d76f5815dbd6cb61ac42878109b6
Marco Puccio <mpuccio90@gmail.com>
2021-05-05 01:25:26 -0300

    improve menu styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,25 +1,27 @@
 import React from "react";
+import "./Menu.css";
 
 export const Menu: React.FC = () => {
   return (
     <nav className="navbar text" role="navigation" aria-label="main navigation">
       <div className="navbar-menu is-justify-content-center">
-        <a className="navbar-item">Home</a>
+        <a className="navbar-item has-text-success">Home</a>
 
-        <a className="navbar-item">LinkedIn</a>
-        <a className="navbar-item">GitHub</a>
+        <a className="navbar-item has-text-info">LinkedIn</a>
+        <a className="navbar-item has-text-grey">GitHub</a>
 
         <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">Archive</a>
-
+          <a className="navbar-link has-text-danger">Archive</a>
           <div className="navbar-dropdown">
-            <a className="navbar-item">2018.marcopuccio.me</a>
-            <a className="navbar-item">2018.marcopuccio.me</a>
-            <hr className="navbar-divider" />
-            <a className="navbar-item">Report an issue</a>
+            <a className="navbar-item has-text-danger">2018.marcopuccio.me</a>
+            <a className="navbar-item has-text-danger">2016.marcopuccio.me</a>
+            <hr className="navbar-divider has-text-danger" />
+            <a className="navbar-item has-text-danger">
+              <small>Report an issue</small>
+            </a>
           </div>
-          <a className="navbar-item">Contact</a>
         </div>
+        <a className="navbar-item has-text-primary">Contact</a>
       </div>
     </nav>
   );
