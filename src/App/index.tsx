45c231657f7a43379df19dcd4df5c5f650211467
Marco Puccio <mpuccio90@gmail.com>
2021-05-08 03:17:00 -0300

    improve styles

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -8,17 +8,19 @@ import { Pitch } from "../components/Pitch";
 
 export const App: React.FC = () => {
   return (
-    <div className="App-container pt-3">
-      <div className="card mx-3">
-        <div className="card-content">
-          <div className="media">
-            <Header />
-          </div>
-          <div className="content">
-            <Menu />
-            <section className="section">
-              <Pitch />
-            </section>
+    <div className="App-root is-flex is-justify-content-center py-3">
+      <div className="App-cardContainer mx-3">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <Header />
+            </div>
+            <div className="content">
+              <Menu />
+              <section className="section">
+                <Pitch />
+              </section>
+            </div>
           </div>
         </div>
       </div>
