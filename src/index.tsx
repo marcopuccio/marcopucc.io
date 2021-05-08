fe55be4fc63c7ec60921015f26acb5acc44145e5
Marco Puccio <mpuccio90@gmail.com>
2021-05-08 01:16:17 -0300

    add font awesome

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import "bulma/css/bulma.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
