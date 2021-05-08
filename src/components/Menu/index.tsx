00ddd932b3cde4ba8d6525fa508ab386bef236ce
Marco Puccio <mpuccio90@gmail.com>
2021-05-07 23:21:17 -0300

    refactor to flex menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,64 +3,64 @@ import "./Menu.css";
 
 export const Menu: React.FC = () => {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-menu is-justify-content-center">
-        <a className="navbar-item has-text-success" href="/">
+    <div className="is-flex is-flex-direction-row is-justify-content-center is-flex-wrap-wrap">
+      <div className="is-flex pr-6">
+        <a className="has-text-success" href="/#">
           Home
         </a>
+      </div>
+      <div className="is-flex pr-6">
         <a
-          className="navbar-item has-text-info"
+          className="has-text-info"
           href="https://www.linkedin.com/in/marcopuccio/"
           target="_blank"
           rel="noreferrer"
         >
           LinkedIn
         </a>
-
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link has-text-danger" href="/">
-            Archive
-          </a>
-          <div className="navbar-dropdown">
-            <a
-              className="navbar-item has-text-danger"
-              href="https://2018.marcopuccio.me/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              2018.marcopuccio.me
-            </a>
-            <a
-              className="navbar-item has-text-danger"
-              href="https://2016.marcopuccio.me/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              2016.marcopuccio.me
-            </a>
-            <hr className="navbar-divider has-text-danger" />
-            <a
-              className="navbar-item has-text-danger"
-              href="https://github.com/marcopuccio/marcopuccio.github.io/issues/new"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <small>Report an issue</small>
-            </a>
-          </div>
-        </div>
+      </div>
+      <div className="is-flex pr-6">
         <a
-          className="navbar-item has-text-grey"
+          className="has-text-grey"
           href="https://github.com/marcopuccio"
           target="_blank"
           rel="noreferrer"
         >
           GitHub
         </a>
-        {/* <a className="navbar-item has-text-primary" href="/">
-          Contact
-        </a> */}
       </div>
-    </nav>
+      {/* <div className="has-dropdown is-hoverable">
+        <a className="has-text-danger" href="/">
+          Archive
+        </a>
+        <div className="navbar-dropdown">
+          <a
+            className="has-text-danger"
+            href="https://2018.marcopuccio.me/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            2018.marcopuccio.me
+          </a>
+          <a
+            className="has-text-danger"
+            href="https://2016.marcopuccio.me/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            2016.marcopuccio.me
+          </a>
+          <hr className="navbar-divider has-text-danger" />
+          <a
+            className="has-text-danger"
+            href="https://github.com/marcopuccio/marcopuccio.github.io/issues/new"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <small>Report an issue</small>
+          </a>
+        </div>
+      </div> */}
+    </div>
   );
 };
