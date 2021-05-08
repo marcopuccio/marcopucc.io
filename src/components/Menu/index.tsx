5289fc0ec5bbff43623357108269492a5e09a547
Marco Puccio <mpuccio90@gmail.com>
2021-05-08 01:59:13 -0300

    improve menu with buttons styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,69 +4,89 @@ import "./Menu.css";
 export const Menu: React.FC = () => {
   return (
     <div className="is-flex is-flex-direction-row is-justify-content-center is-flex-wrap-wrap">
-      <div className="Menu-item is-flex px-4 py-1">
-        <a className="has-text-success" href="/#">
-          Home
+      <div className="is-flex">
+        <a className="button is-primary is-inverted" href="/#">
+          <span>Home</span>
         </a>
       </div>
-      <div className="Menu-item is-flex px-4 py-1">
+      <div className="is-flex">
         <a
-          className="has-text-info"
+          className="button is-info is-inverted"
           href="https://www.linkedin.com/in/marcopuccio/"
           target="_blank"
           rel="noreferrer"
         >
-          LinkedIn
+          <span>LinkedIn</span>
           <span className="icon">
             <i className="fab fa-linkedin"></i>
           </span>
         </a>
       </div>
-      <div className="Menu-item is-flex px-4 py-1">
+      <div className="is-flex">
         <a
-          className="has-text-grey"
+          className="button is-white"
           href="https://github.com/marcopuccio"
           target="_blank"
           rel="noreferrer"
         >
-          GitHub
+          <span>GitHub</span>
           <span className="icon">
             <i className="fab fa-github"></i>
           </span>
         </a>
       </div>
-      {/* <div className="has-dropdown is-hoverable">
-        <a className="has-text-danger" href="/">
-          Archive
-        </a>
-        <div className="navbar-dropdown">
-          <a
-            className="has-text-danger"
-            href="https://2018.marcopuccio.me/"
-            target="_blank"
-            rel="noreferrer"
+
+      {/* <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <button
+            className="button is-inverted is-danger"
+            aria-haspopup="true"
+            aria-controls="archive-dropdown"
           >
-            2018.marcopuccio.me
-          </a>
-          <a
-            className="has-text-danger"
-            href="https://2016.marcopuccio.me/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            2016.marcopuccio.me
-          </a>
-          <hr className="navbar-divider has-text-danger" />
-          <a
-            className="has-text-danger"
-            href="https://github.com/marcopuccio/marcopuccio.github.io/issues/new"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <small>Report an issue</small>
-          </a>
+            <span>Archive</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
         </div>
-      </div> */}
+        <div className="dropdown-menu" id="archive-dropdown" role="menu">
+          <div className="dropdown-content">
+            <div className="dropdown-item">
+              <a
+                className="is-danger is-inverted"
+                href="https://2018.marcopuccio.me/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2018.marcopuccio.me
+              </a>
+            </div>
+            <div className="dropdown-item">
+              <a
+                className="is-danger is-inverted"
+                href="https://2016.marcopuccio.me/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2016.marcopuccio.me
+              </a>
+            </div>
+
+            <hr className="dropdown-divider" />
+            <div className="dropdown-item">
+              <a
+                className="is-danger is-inverted"
+                href="https://github.com/marcopuccio/marcopuccio.github.io/issues/new"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <small>Report an issue</small>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+       */}
     </div>
   );
 };
