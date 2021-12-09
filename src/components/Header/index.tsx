97dd7a86ce8946e848d7633f05c8f56948cd2433
Marco Puccio <mpuccio90@gmail.com>
2021-12-09 02:13:34 -0300

    update title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export const Header: React.FC = () => {
       </div>
       <div className="has-text-centered">
         <Title>Marco Puccio</Title>
-        <Subtitle>Software Engineer & Professor</Subtitle>
+        <Subtitle>Software Architect & Professor</Subtitle>
       </div>
     </header>
   );
