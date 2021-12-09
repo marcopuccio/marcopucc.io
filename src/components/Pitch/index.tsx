1573d70ec9367fe60bb5995e0981b4b1f0945577
Marco Puccio <mpuccio90@gmail.com>
2021-12-09 02:11:58 -0300

    update pitch

--- src/components/Pitch/index.tsx
+++ src/components/Pitch/index.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 export const Pitch: React.FC = () => {
   return (
-    <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
-      I have a strong experience in management and software engineering, focus
-      on architecture and innovation. I’m passionate about learning and
-      teaching, and that’s why I think that knowledge doesn’t belong to anyone,
-      so it must be shared with others. My goal is to keep growing and learning
-      about business and product development to reach people and satisfy their
-      needs.
-    </p>
+    <>
+      <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
+        I have a strong experience in tech management, software architecture and
+        design.
+      </p>
+      <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
+        I’m passionate about learning and teaching, and that’s why I think that
+        knowledge doesn’t belong to anyone, so it must be shared with others.
+      </p>
+      <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
+        My goal is to keep learning and growing professionally by driving value
+        with innovative solutions and new technologies to satisfy people modern
+        needs.
+      </p>
+    </>
   );
 };
