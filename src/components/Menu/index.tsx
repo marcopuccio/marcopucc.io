5274557d98ac33b4e9d209e0e1ea16ae46213c89
Marco Puccio <mpuccio90@gmail.com>
2022-09-27 22:07:41 -0300

    remove archive from header

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { MenuItem, MenuItemProps } from "../MenuItem";
 import { FAIcon } from "../FAIcon";
@@ -35,69 +35,12 @@ const menuItems: MenuItemProps[] = [
   },
 ];
 
-const archiveDropdownMenuItems: MenuItemProps[] = [
-  {
-    color: "is-danger",
-    external: true,
-    href: "https://2018.marcopucc.io/",
-    children: <span>2018.marcopucc.io</span>,
-  },
-  {
-    color: "is-danger",
-    external: true,
-    href: "https://2016.marcopucc.io/",
-    children: <span>2016.marcopucc.io</span>,
-  },
-  {
-    color: "is-danger",
-    external: true,
-    href: "https://github.com/marcopuccio/marcopucc.io/issues/new",
-    children: <small>Report an issue</small>,
-  },
-];
-
 export const Menu: React.FC = () => {
-  const [archiveDropdownActive, setArchiveDropdownActive] =
-    useState<boolean>(false);
-
-  const toggleArchiveDropdown = () => {
-    setArchiveDropdownActive((isActive) => !isActive);
-  };
-
   return (
     <div className="is-flex is-flex-direction-row is-justify-content-center is-flex-wrap-wrap">
       {menuItems.map((item, index) => (
         <MenuItem {...item} key={index} />
       ))}
-
-      <div
-        className={`dropdown is-right ${
-          !!archiveDropdownActive ? "is-active" : ""
-        }`}
-      >
-        <div className="dropdown-trigger">
-          <button
-            className="button is-inverted is-danger"
-            aria-haspopup="true"
-            aria-controls="archive-dropdown"
-            onClick={toggleArchiveDropdown}
-          >
-            <span>Archive</span>
-            <span className="icon is-small">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-        <div className="dropdown-menu" id="archive-dropdown" role="menu">
-          <div className="dropdown-content">
-            {archiveDropdownMenuItems.map((item, index) => (
-              <div className="dropdown-item" key={index}>
-                <MenuItem {...item} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
