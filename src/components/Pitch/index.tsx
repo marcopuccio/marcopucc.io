79f038ca0396885f6f4f377a2ba4c81769813158
Marco Puccio <mpuccio90@gmail.com>
2024-12-23 17:19:11 -0300

    Update index.tsx

--- src/components/Pitch/index.tsx
+++ src/components/Pitch/index.tsx
@@ -2,19 +2,9 @@ import React from "react";
 
 export const Pitch: React.FC = () => {
   return (
-    <>
-      <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
-        I have a strong experience in tech management, software architecture and
-        design.
-      </p>
-      <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
-        I’m passionate about learning and teaching, and that’s why I think that
-        knowledge should be shared with others.
-      </p>
       <p className="is-size-5 has-text-justified has-text-weight-light is-family-code">
         My goal is to drive innovative solutions with technology to satisfy
         people modern needs.
       </p>
-    </>
   );
 };
